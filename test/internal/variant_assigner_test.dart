59e59a28d0a183e9f9a3088fe7480d5031023b0f
Hamza Ali <hamzali4642@gmail.com>
2023-01-10 16:08:25 +0500

    test cases

--- test/internal/variant_assigner_test.dart
+++ test/internal/variant_assigner_test.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:ab_smartly/internal/hashing/hashing.dart';
 import 'package:ab_smartly/internal/variant_assigner.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -47,6 +50,64 @@ void main(){
         VariantAssigner.chooseVariant([0.0, 1.0], 0.0), equals(1));
     expect(
         VariantAssigner.chooseVariant([0.0, 1.0], 1.0), equals(1));
+  });
+
+
+
+  test('testAssignmentsMatch', (){
+
+    Map<dynamic, dynamic> data = {
+      123456789 : [1, 0, 1, 1, 1, 0, 0, 2, 1, 2, 2, 2, 0, 0],
+      "bleh@absmartly.com" : [0, 1, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 1, 1],
+      "e791e240fcd3df7d238cfc285f475e8152fcc0ec" : [1, 0, 1, 1, 0, 0, 0, 2, 0, 2, 1, 0, 0, 1],
+    };
+
+    data.forEach((key, value) {
+
+      var unitUID = key;
+      var expectedVariants = value;
+      final List<List<double>> splits = [
+        [0.5, 0.5],
+        [0.5, 0.5],
+        [0.5, 0.5],
+        [0.5, 0.5],
+        [0.5, 0.5],
+        [0.5, 0.5],
+        [0.5, 0.5],
+        [0.33, 0.33, 0.34],
+        [0.33, 0.33, 0.34],
+        [0.33, 0.33, 0.34],
+        [0.33, 0.33, 0.34],
+        [0.33, 0.33, 0.34],
+        [0.33, 0.33, 0.34],
+        [0.33, 0.33, 0.3],
+      ];
+
+      final List<List<int>> seeds = [
+        [0x00000000, 0x00000000],
+        [0x00000000, 0x00000001],
+        [0x8015406f, 0x7ef49b98],
+        [0x3b2e7d90, 0xca87df4d],
+        [0x52c1f657, 0xd248bb2e],
+        [0x865a84d0, 0xaa22d41a],
+        [0x27d1dc86, 0x845461b9],
+        [0x00000000, 0x00000000],
+        [0x00000000, 0x00000001],
+        [0x8015406f, 0x7ef49b98],
+        [0x3b2e7d90, 0xca87df4d],
+        [0x52c1f657, 0xd248bb2e],
+        [0x865a84d0, 0xaa22d41a],
+        [0x27d1dc86, 0x845461b9]];
+
+      final List<int> unitHash = Hashing.hashUnit(unitUID.toString());
+      final VariantAssigner assigner = VariantAssigner(Uint8List.fromList(unitHash));
+      for (int i = 0; i < seeds.length; ++i) {
+        final List<int> frags = seeds[i];
+        final List<double> split = splits[i];
+        final int variant = assigner.assign(split, frags[0], frags[1]);
+        assert(expectedVariants[i] == variant);
+      }
+    });
   });
 }
 
