84307e1896eb2bb8dcaa5605fa8047adf62daffd
Hamza <hamzali4642@gmail.com>
2023-06-14 14:52:23 +0500

    read_me

--- lib/context_event_logger.dart
+++ lib/context_event_logger.dart
@@ -3,10 +3,9 @@ import 'package:mockito/annotations.dart';
 
 // @GenerateNiceMocks([MockSpec<ContextEventLogger>()])
 //
-// abstract class ContextEventLogger {
-//   void handleEvent(Context context, EventType type, dynamic data);
-// }
-
+abstract class ContextEventLogger {
+  void handleEvent(Context context, EventType type, dynamic data);
+}
 enum EventType {
   Error,
   Ready,
